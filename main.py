f505a3bd1e4b6ad37fe0d67703f9acccb7ab6f52
and_wu <and36uwu@gmail.com>
2025-08-08 18:31:11 +0300

    всю логику работы с users пынес в отдельные модули и положил их в папку users

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from _pytest.nodes import Item
 from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel, EmailStr
 from items_views import router as items_router
-
+from users.views import router as users_router
 app = FastAPI()
 app.include_router(items_router)
-
-class CreateUser(BaseModel):
-    email: EmailStr
-
+app.include_router(users_router)
 
 @app.get("/")
 def hello_index():
@@ -24,12 +19,6 @@ def hello(name: str = "World"):
     return {"message": f"Hello {name}!"}
 
 
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return {
-        "message": "success",
-        "email": user.email,
-    }
 
 
 @app.get("/calc/add/")
